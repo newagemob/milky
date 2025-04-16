f010d482f5fb35d0b05b676d5fe6ffdc5836fe5d
Isaiah Wright <82986991+newagemob@users.noreply.github.com>
2025-04-16 17:11:50 -0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+    ...compat.config({
+    extends: ['next'],
     rules: {
       // Add looser linting rules here
       "no-unused-vars": "warn",
       "no-console": "off",
       "@next/next/no-img-element": "off",
-      "react/no-unescaped-entities": "off",
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
       "react-hooks/exhaustive-deps": "warn"
     },
-  },
+  })
 ];
 
 export default eslintConfig;
