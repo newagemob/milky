e4b7ed677d70df8a7f89f391dafdf22a4072a0ae
Isaiah Wright <82986991+newagemob@users.noreply.github.com>
2025-06-08 23:52:27 -0500

    Update MilkyFireR3F.tsx

--- components/glsl/MilkyFireR3F.tsx
+++ components/glsl/MilkyFireR3F.tsx
@@ -4,7 +4,7 @@ import { useRef } from "react"
 import { useFrame } from "@react-three/fiber"
 import * as THREE from "three"
 
-export function CosmicFireShader() {
+export function MilkyFireR3F() {
   const materialRef = useRef<THREE.ShaderMaterial>(null)
 
   useFrame((state) => {
