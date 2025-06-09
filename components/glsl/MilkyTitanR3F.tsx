23e400c08af45dd9925f04c1775b08cd8c3c82ba
Isaiah Wright <82986991+newagemob@users.noreply.github.com>
2025-06-08 23:15:52 -0500

    Update MilkyTitanR3F.tsx

--- components/glsl/MilkyTitanR3F.tsx
+++ components/glsl/MilkyTitanR3F.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react"
 import { useFrame } from "@react-three/fiber"
 import * as THREE from "three"
 
-export function RaymarchedShaderR3F() {
+export function MilkyTitanR3F() {
   const materialRef = useRef<THREE.ShaderMaterial>(null)
 
   useFrame((state) => {
